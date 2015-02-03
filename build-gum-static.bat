16af4d59dc25a50ce7a5bc7a26090247fac61197
Mihai Cimpoesu <mihai@jivaro.com>
2015-02-03 20:03:37 +0200

    Updating windows build script

--- build-gum-static.bat
+++ build-gum-static.bat
@@ -1,3 +1,2 @@
 @echo off
-set DEVENV="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
 "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" frida.sln /build release /project gumpp-static
