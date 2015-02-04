bd265cfa7e624ed75f99f31b030ee52074c5cbeb
Mihai Cimpoesu <mihai@jivaro.com>
2015-02-04 14:42:35 +0200

    Switch to msbuild for automatic building

--- build-gum-static.bat
+++ build-gum-static.bat
@@ -1,2 +1,7 @@
 @echo off
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" frida.sln /build release /project gumpp-static
+
+git submodule init
+git submodule update
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+call msbuild frida.sln /p:Configuration=Release;Platform=Win32 /t:gumpp-static:Rebuild /maxcpucount:4
