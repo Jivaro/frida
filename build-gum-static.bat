de54521eea1010aca6457cc6cbe64d96a1b419f7
Mihai Cimpoesu <mihai@jivaro.com>
2015-02-05 15:53:37 +0100

    Adding debug build for frida libs

--- build-gum-static.bat
+++ build-gum-static.bat
@@ -4,4 +4,4 @@ git submodule init
 git submodule update
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
-call msbuild frida.sln /p:Configuration=Release;Platform=Win32 /t:gumpp-static:Rebuild /maxcpucount:4
+call msbuild frida.sln /p:Configuration=%1;Platform=Win32 /t:gumpp-static:Rebuild /maxcpucount:4
--- package-frida-libs.bat
+++ package-frida-libs.bat
@@ -8,16 +8,10 @@ MD %PACKAGE_DIR%
 MD %PACKAGE_DIR%\lib
 MD %PACKAGE_DIR%\lib\win32
 MD %PACKAGE_DIR%\lib\win32\release
+MD %PACKAGE_DIR%\lib\win32\debug
 
-set RELEASE_LIB_DIR=%PACKAGE_DIR%\lib\win32\release
-echo %RELEASE_LIB_DIR%
 
-COPY /Y build\frida-windows\Win32-Release\bin\gumpp-static.* 		%RELEASE_LIB_DIR%
-COPY /Y build\tmp-windows\Win32-Release\gum-32\gum-32.pdb 		%RELEASE_LIB_DIR%
-COPY /Y build\tmp-windows\Win32-Release\capstone-32\capstone-32.pdb 	%RELEASE_LIB_DIR%
-COPY /Y build\sdk-windows\Win32-Release\lib\*.lib			%RELEASE_LIB_DIR%	 
-COPY /Y build\sdk-windows\Win32-Release\lib\*.pdb			%RELEASE_LIB_DIR%
-DEL  /F /Q %RELEASE_LIB_DIR%\v8*
+@echo "Copy all include files to target folder"
 
 MD %PACKAGE_DIR%\include   
 MD %PACKAGE_DIR%\include\gum   
@@ -40,3 +34,32 @@ MD %PACKAGE_DIR%\include\libffi-3.1-rc1\include
 
 XCOPY /Y /S /Q  build\sdk-windows\Win32-Release\lib\libffi-3.1-rc1\include\*  %PACKAGE_DIR%\include\libffi-3.1-rc1\include\
 XCOPY /Y /S /Q  frida-gum\bindings\gumpp\gumpp.hpp %PACKAGE_DIR%\include\
+
+
+
+@echo "Copy all release lib files to target folder"
+
+set RELEASE_LIB_DIR=%PACKAGE_DIR%\lib\win32\release
+echo %RELEASE_LIB_DIR%
+
+COPY /Y build\frida-windows\Win32-Release\bin\gumpp-static.lib 		%RELEASE_LIB_DIR%
+COPY /Y build\frida-windows\Win32-Release\bin\gumpp-static.pdb 		%RELEASE_LIB_DIR%
+COPY /Y build\tmp-windows\Win32-Release\gum-32\gum-32.pdb 		%RELEASE_LIB_DIR%
+COPY /Y build\tmp-windows\Win32-Release\capstone-32\capstone-32.pdb 	%RELEASE_LIB_DIR%
+COPY /Y build\sdk-windows\Win32-Release\lib\*.lib			%RELEASE_LIB_DIR%	 
+COPY /Y build\sdk-windows\Win32-Release\lib\*.pdb			%RELEASE_LIB_DIR%
+DEL  /F /Q %RELEASE_LIB_DIR%\v8*
+
+@echo "Copy all debug lib files to target folder"
+
+
+set RELEASE_LIB_DIR=%PACKAGE_DIR%\lib\win32\debug
+echo %RELEASE_LIB_DIR%
+
+COPY /Y build\frida-windows\Win32-Debug\bin\gumpp-static.lib 		%RELEASE_LIB_DIR%
+COPY /Y build\frida-windows\Win32-Debug\bin\gumpp-static.pdb 		%RELEASE_LIB_DIR%
+COPY /Y build\tmp-windows\Win32-Debug\gum-32\gum-32.pdb 		%RELEASE_LIB_DIR%
+COPY /Y build\tmp-windows\Win32-Debug\capstone-32\capstone-32.pdb 	%RELEASE_LIB_DIR%
+COPY /Y build\sdk-windows\Win32-Debug\lib\*.lib			%RELEASE_LIB_DIR%	 
+COPY /Y build\sdk-windows\Win32-Debug\lib\*.pdb			%RELEASE_LIB_DIR%
+DEL  /F /Q %RELEASE_LIB_DIR%\v8*
